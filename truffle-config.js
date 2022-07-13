edd242bffc349c8dd7792f641166428ea7b42d73
sunjianguo <jianguo.sun@newtouch.com>
2022-07-13 16:16:16 +0800

    change truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -25,7 +25,15 @@ module.exports = {
        confirmations: 2,    // # of confs to wait between deployments. (default: 0)
        timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
        skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-   }
+   },
+  rinkeby_node: {
+      provider: () => new HDWalletProvider(testMnemonic, `https://eth-rinkeby.alchemyapi.io/v2/aVUIrFeeNyFD0T87zh6_iZ4Ws_tghX7A`),
+      network_id: 4,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
+  },
   },
   //
   // Truffle DB is currently disabled by default; to enable it, change enabled:
